aad2227c1bbc5a7cd66c233d81c7246acfb11b90
Kirill Grigorev <grigorev@posteo.de>
2026-04-15 00:35:36 +0200

    feat: expose method to get a configured HTTP handler from the app

--- app.go
+++ app.go
@@ -8,6 +8,7 @@ import (
 	"log"
 	"net"
 	"net/http"
+	"net/netip"
 	"os"
 	"reflect"
 	"strings"
@@ -31,7 +32,7 @@ import (
 //go:generate go tool mockery
 
 const (
-	// FailedShutdownExitCode is returned when application cannot accomplish graceful shutdown
+	// FailedShutdownExitCode is returned when the application cannot achieve a graceful shutdown
 	FailedShutdownExitCode = 130
 
 	// ServerReadHeaderTimeout default timeout to read incoming request headers
@@ -82,7 +83,7 @@ func DefaultContext(name string) ApplicationOption {
 	}
 }
 
-// SetEagerSingletons controls if eager singletons will be created
+// SetEagerSingletons controls if eager singletons are created
 func SetEagerSingletons(enabled bool) ApplicationOption {
 	return func(config *Application) {
 		config.eagerSingletons = enabled
@@ -103,7 +104,7 @@ func WithRoutes(routesModule web.RoutesModule) ApplicationOption {
 	}
 }
 
-// WithCustomLogger allows to use custom logger modules for flamingo app, if nothing available default will be used
+// WithCustomLogger allows using custom logger modules for flamingo app, if nothing available default will be used
 func WithCustomLogger(logger dingo.Module) ApplicationOption {
 	return func(config *Application) {
 		config.loggerModule = logger
@@ -290,6 +291,26 @@ func (app *Application) Run() error {
 	return nil
 }
 
+// HTTPHandler -
+func (app *Application) HTTPHandler() (http.Handler, error) {
+	injector, err := app.area.GetInitializedInjector()
+	if err != nil {
+		return nil, fmt.Errorf("%w: couldn't get initialized injector: %w", ErrAppRun, err)
+	}
+
+	i, err := injector.GetInstance(new(http.Handler))
+	if err != nil {
+		return nil, fmt.Errorf("%w: couldn't get http.Handler: %w", ErrAppRun, err)
+	}
+
+	handler, ok := i.(http.Handler)
+	if !ok {
+		return nil, fmt.Errorf("%w: resolved type for a handler is not http.Handler", ErrAppRun)
+	}
+
+	return handler, nil
+}
+
 func typeName(of reflect.Type) string {
 	var name string
 
@@ -405,11 +426,13 @@ func (sm *servemodule) Inject(
 func (sm *servemodule) Configure(injector *dingo.Injector) {
 	flamingo.BindEventSubscriber(injector).ToInstance(sm)
 
-	injector.BindMulti(new(cobra.Command)).ToProvider(func(opts *struct {
+	injector.Bind(new(http.Handler)).ToProvider(func(opts *struct {
 		Handler flamingoHttp.HandlerWrapper `inject:",optional"`
-	}) *cobra.Command {
-		return sm.serveProvider(opts.Handler)
+	}) http.Handler {
+		return sm.handlerProvider(opts.Handler)
 	})
+
+	injector.BindMulti(new(cobra.Command)).ToProvider(sm.serveProvider)
 }
 
 // CueConfig for the module
@@ -417,7 +440,19 @@ func (sm *servemodule) CueConfig() string {
 	return `core: serve: port: >= 0 & <= 65535 | *3322`
 }
 
-func (sm *servemodule) serveProvider(handlerWrapper flamingoHttp.HandlerWrapper) *cobra.Command {
+func (sm *servemodule) handlerProvider(handlerWrapper flamingoHttp.HandlerWrapper) http.Handler {
+	sm.mu.Lock()
+	sm.mu.Unlock()
+
+	var handler = sm.router.Handler()
+	if handlerWrapper != nil {
+		handler = handlerWrapper(handler)
+	}
+
+	return handler
+}
+
+func (sm *servemodule) serveProvider(handler http.Handler) *cobra.Command {
 	serveCmd := &cobra.Command{
 		Use:   "serve",
 		Short: "Default serve command - starts on Port 3322",
@@ -426,10 +461,7 @@ func (sm *servemodule) serveProvider(handlerWrapper flamingoHttp.HandlerWrapper)
 				sm.mu.Lock()
 				defer sm.mu.Unlock()
 
-				sm.server.Handler = sm.router.Handler()
-				if handlerWrapper != nil {
-					sm.server.Handler = handlerWrapper(sm.server.Handler)
-				}
+				sm.server.Handler = handler
 			}()
 
 			err := sm.listenAndServe()
@@ -458,7 +490,12 @@ func (sm *servemodule) listenAndServe() error {
 	addr := listener.Addr().String()
 	sm.logger.Info(fmt.Sprintf("Starting HTTP Server at %s .....", addr))
 
-	port := addr[strings.LastIndex(addr, ":")+1:]
+	addrPort, err := netip.ParseAddrPort(addr)
+	if err != nil {
+		sm.logger.Error(err.Error())
+	}
+
+	port := fmt.Sprint(addrPort.Port())
 
 	sm.eventRouter.Dispatch(context.Background(), &flamingo.ServerStartEvent{Port: port})
 	defer sm.eventRouter.Dispatch(context.Background(), &flamingo.ServerShutdownEvent{})
